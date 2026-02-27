1a05a8b18e5d49a23b09da01c94c38dda99aeef5
osuelflo <owensuelflow@gmail.com>
2026-02-26 18:41:59 -0600

    back to 2500

--- code/oh_hell_server.py
+++ code/oh_hell_server.py
@@ -270,7 +270,7 @@ def run_bidding_phase(state, human_player=0):
 
     return messages
 
-def run_playing_phase(state, human_player=0, iterations=100):
+def run_playing_phase(state, human_player=0, iterations=2500):
     messages = []
     if state.GetMoves() == 0:
         return messages
